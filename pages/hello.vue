d8c5d3a6d177e45b4d5de619bd0582dd868b2669
masyngm <sincaponica@gmail.com>
2021-02-25 08:07:06 +0900

    initial commit

---
+++ pages/hello.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">HELLO WORLD!!</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model="newMessage" type="text" />
+    <button @click="addMessage()">追加</button>
+    <button @click="deleteMessage()">古い順に削除</button>
+    <div v-for="message in messages" :key="message.index">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    newMessage: '',
+    messages: [] as any,
+  }),
+  methods: {
+    addMessage() {
+      this.messages.push(this.newMessage)
+      this.newMessage = ''
+    },
+    deleteMessage() {
+      if (this.messages.length > 0) {
+        this.messages.slice(0, 1)
+      }
+    },
+  },
+})
+</script>
